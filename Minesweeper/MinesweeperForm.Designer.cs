c16e3a7230a9c1f74154dd20e6470d242f50c936
Humaeed Ahmed <humaeedanik@gmail.com>
2018-04-27 05:51:19 +0600

    1st Commit ~

---
+++ Minesweeper/MinesweeperForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Minesweeper
+{
+    partial class MinesweeperForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MinesweeperForm));
+            this.Exit = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.Watch = new System.Windows.Forms.Label();
+            this.WatchLabel = new System.Windows.Forms.Label();
+            this.Pause = new System.Windows.Forms.Button();
+            this.FlagLabel = new System.Windows.Forms.Label();
+            this.Flag = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Consolas", 13F, System.Drawing.FontStyle.Bold);
+            this.Exit.ForeColor = System.Drawing.Color.DimGray;
+            this.Exit.Location = new System.Drawing.Point(444, 317);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(86, 39);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "EXIT";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.ExitButtonClick);
+            // 
+            // Play
+            // 
+            this.Play.Font = new System.Drawing.Font("Consolas", 13F, System.Drawing.FontStyle.Bold);
+            this.Play.ForeColor = System.Drawing.Color.DimGray;
+            this.Play.Location = new System.Drawing.Point(444, 207);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(86, 39);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "PLAY";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.PlayButtonClick);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Title.Location = new System.Drawing.Point(422, 16);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(131, 23);
+            this.Title.TabIndex = 66;
+            this.Title.Text = "MINESWEEPER";
+            // 
+            // Watch
+            // 
+            this.Watch.AutoSize = true;
+            this.Watch.Font = new System.Drawing.Font("Consolas", 10F);
+            this.Watch.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Watch.Location = new System.Drawing.Point(455, 158);
+            this.Watch.Name = "Watch";
+            this.Watch.Size = new System.Drawing.Size(64, 17);
+            this.Watch.TabIndex = 67;
+            this.Watch.Text = "00 : 00";
+            // 
+            // WatchLabel
+            // 
+            this.WatchLabel.AutoSize = true;
+            this.WatchLabel.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WatchLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.WatchLabel.Location = new System.Drawing.Point(455, 140);
+            this.WatchLabel.Name = "WatchLabel";
+            this.WatchLabel.Size = new System.Drawing.Size(56, 18);
+            this.WatchLabel.TabIndex = 68;
+            this.WatchLabel.Text = " TIMER";
+            // 
+            // Pause
+            // 
+            this.Pause.Font = new System.Drawing.Font("Consolas", 13F, System.Drawing.FontStyle.Bold);
+            this.Pause.ForeColor = System.Drawing.Color.DimGray;
+            this.Pause.Location = new System.Drawing.Point(444, 262);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(86, 39);
+            this.Pause.TabIndex = 69;
+            this.Pause.Text = "PAUSE";
+            this.Pause.UseVisualStyleBackColor = true;
+            this.Pause.Click += new System.EventHandler(this.PauseButtonClick);
+            // 
+            // FlagLabel
+            // 
+            this.FlagLabel.AutoSize = true;
+            this.FlagLabel.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FlagLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.FlagLabel.Location = new System.Drawing.Point(464, 82);
+            this.FlagLabel.Name = "FlagLabel";
+            this.FlagLabel.Size = new System.Drawing.Size(40, 18);
+            this.FlagLabel.TabIndex = 71;
+            this.FlagLabel.Text = "FLAG";
+            // 
+            // Flag
+            // 
+            this.Flag.AutoSize = true;
+            this.Flag.Font = new System.Drawing.Font("Consolas", 10F);
+            this.Flag.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Flag.Location = new System.Drawing.Point(472, 100);
+            this.Flag.Name = "Flag";
+            this.Flag.Size = new System.Drawing.Size(24, 17);
+            this.Flag.TabIndex = 70;
+            this.Flag.Text = "10";
+            // 
+            // MinesweeperForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(580, 396);
+            this.Controls.Add(this.FlagLabel);
+            this.Controls.Add(this.Flag);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.WatchLabel);
+            this.Controls.Add(this.Watch);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MinesweeperForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minesweeper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label Watch;
+        private System.Windows.Forms.Label WatchLabel;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Label FlagLabel;
+        private System.Windows.Forms.Label Flag;
+    }
+}
+
---
+++ Minesweeper/MinesweeperForm.cs
@@ -0,0 +1,462 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minesweeper
+{
+    public partial class MinesweeperForm : Form
+    {
+        struct Timex
+        {
+            public int minute, second;
+            public Timex(int m, int s)
+            {
+                minute = m;
+                second = s;
+            }
+        }
+
+        // Stuff
+        bool game_flag = false, game_start = false, game_pause = false;
+        Dictionary<Button, int> ajx = new Dictionary<Button, int>();
+        Dictionary<Button, int> flx = new Dictionary<Button, int>();
+        Button[,] box = new Button[8, 8];
+        int[,] mines = new int[8, 8];
+        Timex timer = new Timex(0, 0);
+        Random rand = new Random();
+        int flags = 10;
+        Timer runtime;
+
+        public MinesweeperForm()
+        {
+            InitializeComponent();
+            Pause.Enabled = false;
+            Add_Boxes();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+
+                }
+            }
+        }
+
+        void ExitButtonClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        } 
+
+        void Start_Game() // Could use a timer here to run the game
+        {
+            runtime = new Timer();
+            runtime.Enabled = true;
+            runtime.Interval = 1000;
+            runtime.Tick += Runtime_Tick;
+            Pause.Enabled = true;
+        }
+
+        void Game_Reset() // After clicking replay button
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    box[i, j].Enabled = true;
+                    box[i, j].BackColor = SystemColors.Control;
+                }
+            }
+
+            if (game_start) runtime.Dispose();
+            Pause.Enabled = game_pause = false;
+            timer.minute = timer.second = 0;
+            Watch.Text = "00 : 00";
+            Flag.Text = "10";
+            Pause.Text = "PAUSE";
+            game_start = false;
+            flags = 10;
+            ajx.Clear();
+            for (int i = 0; i < 8; i++) for (int j = 0; j < 8; j++) flx[box[i, j]] = 0;
+            Reset_Mines();
+        }
+
+        void Add_Boxes() // Adding buttons to form in runtime
+        {
+            int x = 25, y = 16;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    box[i, j] = new Button();
+                    box[i, j].Font = new Font("Consolas", 12F, FontStyle.Bold);
+                    box[i, j].Click += new EventHandler(BoxButtonClick);
+                    box[i, j].MouseDown += new MouseEventHandler(MouseDownBox);
+                    box[i, j].BackColor = SystemColors.Control;
+                    box[i, j].FlatStyle = FlatStyle.Flat;
+                    box[i, j].FlatAppearance.BorderSize = 0;
+                    box[i, j].UseVisualStyleBackColor = false;
+                    box[i, j].Location = new Point(x, y);
+                    box[i, j].Size = new Size(40, 40);
+                    box[i, j].TabIndex = 2;
+                    Controls.Add(box[i,j]);
+                    flx[box[i, j]] = 0;
+                    x += 46;
+                }
+                x = 25;
+                y += 46;
+            }
+        }
+
+        void Set_Mines() // Setting mines in the grid randomly
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                int[] visit = new int[8];
+                int n = rand.Next(1, 3), x = 0; // Difficulty controlled by upperbound
+
+                while (x < n)
+                {
+                    int z = rand.Next(0, 8);
+                    if (visit[z] == 0)
+                    {
+                        // box[i, z].Text = "×";
+                        mines[i, z] = 1;
+                        visit[z] = 1;
+                        x++;
+                    }
+                }
+            }
+
+            // Set mine adjacencies
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    int c = 0;
+                       
+                    if (i + 1 < 8)
+                    {
+                        if (mines[i + 1, j] == 1) c++;
+                    }
+                    if (i - 1 >= 0)
+                    {
+                        if (mines[i - 1, j] == 1) c++;
+                    }
+                    if (j + 1 < 8)
+                    {
+                        if (mines[i, j + 1] == 1) c++;
+                    }
+                    if (j - 1 >= 0)
+                    {
+                        if (mines[i, j - 1] == 1) c++;
+                    }/*
+                    if (i + 1 < 8 && j + 1 < 8)
+                    {
+                        if (mines[i + 1, j + 1] == 1) c++;
+                    }
+                    if (i + 1 < 8 && j - 1 >= 0)
+                    {
+                        if (mines[i + 1, j - 1] == 1) c++;
+                    }
+                    if (i - 1 >= 0 && j + 1 < 8)
+                    {
+                        if (mines[i - 1, j + 1] == 1) c++;
+                    }
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                    {
+                        if (mines[i - 1, j - 1] == 1) c++;
+                    }*/
+
+                    ajx[box[i, j]] = c;
+                    // box[i, j].Text = c.ToString();
+                }
+            }
+        }
+
+        void Reset_Mines() // Making mines every index zero
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    mines[i, j] = 0;
+                    box[i, j].Text = "";
+                }
+            }
+            Set_Mines();
+        }
+
+        bool Is_Mine(Button b) // Checks if the selected box is a mine or not
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (box[i, j] == b)
+                    {
+                        if (mines[i, j] == 0)
+                            return false;
+                        else
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+       
+        bool Win()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (mines[i, j] == 1 || flx[box[i, j]] == 1)
+                        continue;
+                    else
+                    {
+                        if (box[i, j].Enabled)
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        void BoxButtonClick(object sender, EventArgs e)
+        {
+            if (!game_pause)
+            {
+                if (game_flag)
+                {
+                    Button b = (Button)sender;
+
+                    if (flx[b] == 1) return;
+
+                    if (Is_Mine(b))
+                    {
+                        for (int i = 0; i < 8; i++)
+                        {
+                            for (int j = 0; j < 8; j++)
+                            {
+                                if (mines[i, j] == 1)
+                                {
+                                    box[i, j].Text = "💀";
+                                    box[i, j].Enabled = false;
+                                    box[i, j].BackColor = Color.LightGray;
+                                    box[i, j].Font = new Font("Consolas", 15F, FontStyle.Bold);
+                                }
+                            }
+                        }
+
+                        if (game_start) runtime.Dispose();
+                        Pause.Enabled = false;
+                        game_pause = true;
+                    }
+                    else
+                    {
+                        if (ajx[b] != 0)
+                        {
+                            b.Enabled = false;
+                            b.Text = ajx[b].ToString();
+                            b.BackColor = Color.LightGray;
+
+                        }
+                        else
+                        {
+                            Special_Clear(b);
+                        }
+
+                        if (!game_start)
+                        {
+                            game_start = true;
+                            Start_Game();
+                        }
+                    }
+                }
+            }
+
+            if (Win())
+            {
+                // If Player wins
+            }
+        }
+
+        void MouseDownBox(object sender, MouseEventArgs e)
+        {
+            if (!game_pause)
+            {
+                if (game_flag)
+                {
+                    Button b = (Button)sender;
+                    if (e.Button == MouseButtons.Right)
+                    {
+                        if (flx[b] == 0)
+                        {
+                            if (flags != 0)
+                            {
+                                flags--;
+                                flx[b] = 1;
+                                b.Text = "🚩";
+                                Flag.Text = flags.ToString();
+                                b.Font = new Font("Consolas", 15F, FontStyle.Bold);
+                            }
+                        }
+                        else
+                        {
+                            flags++;
+                            flx[b] = 0;
+                            b.Text = "";
+                            Flag.Text = flags.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        void Special_Clear(Button b)
+        {
+            int x = 0, y = 0, up_i = 0, down_i = 7;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (box[i, j].Equals(b))
+                    {
+                        x = i; y = j;
+                    }
+                }
+            }
+
+            for (int i = x + 1; i < 8; i++) // checking lower limit
+            {
+                if (ajx[box[i, y]] != 0)
+                {
+                    down_i = i;
+                    break;
+                }
+            }
+
+            for (int i = x - 1; i >= 0; i--) // checking upper limit
+            {
+                if (ajx[box[i, y]] != 0)
+                {
+                    up_i = i;
+                    break;
+                }
+            }
+
+
+            for (int i = up_i; i <= down_i; i++)
+            {
+                for (int j = y; j < 8; j++)
+                {
+                    if (flx[box[i, j]] == 1) continue;
+
+                    if (ajx[box[i, j]] != 0)
+                    {
+                        box[i, j].Enabled = false;
+                        box[i, j].Text = ajx[box[i, j]].ToString();
+                        box[i, j].BackColor = Color.LightGray;
+                        break;
+                    }
+                    else
+                    {
+                        box[i, j].Enabled = false;
+                        box[i, j].BackColor = Color.LightGray;
+                    }
+                }
+
+                for (int j = y; j >= 0; j--)
+                {
+                    if (flx[box[i, j]] == 1) continue;
+
+                    if (ajx[box[i, j]] != 0)
+                    {
+                        box[i, j].Enabled = false;
+                        box[i, j].Text = ajx[box[i, j]].ToString();
+                        box[i, j].BackColor = Color.LightGray;
+                        break;
+                    }
+                    else
+                    {
+                        box[i, j].Enabled = false;
+                        box[i, j].BackColor = Color.LightGray;
+                    }
+                }
+            }
+        }
+
+        void PlayButtonClick(object sender, EventArgs e)
+        {
+            if (!game_flag)
+            {
+                Play.Text = "REPLAY";
+                game_flag = true;
+                Set_Mines();
+            }
+            else
+            {
+                Game_Reset();
+            }
+        }
+
+        void PauseButtonClick(object sender, EventArgs e)
+        {
+            if (game_pause)
+            {
+                game_pause = false;
+                Pause.Text = "PAUSE";
+            }
+            else
+            {
+                game_pause = true;
+                Pause.Text = "PLAY";
+            }
+        }
+
+        void Runtime_Tick(object sender, EventArgs e)
+        {
+            if (!game_pause)
+            {
+                timer.second++;
+                if (timer.second == 60)
+                {
+                    timer.second = 0;
+                    timer.minute++;
+                }
+
+                string mm = timer.minute.ToString(), ss = timer.second.ToString();
+                if (mm.Length == 1)
+                {
+                    if (ss.Length == 1)
+                    {
+                        Watch.Text = "0" + mm + " : 0" + ss;
+                    }
+                    else
+                    {
+                        Watch.Text = "0" + mm + " : " + ss;
+                    }
+                }
+                else
+                {
+                    if (ss.Length == 1)
+                    {
+                        Watch.Text = mm + " : 0" + ss;
+                    }
+                    else
+                    {
+                        Watch.Text = mm + " : " + ss;
+                    }
+                }
+            }
+        }
+    }
+}
+
+// 🚩 💀 💥
